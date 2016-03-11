73c5a449e3c2147366063f3d5f530309dfb27393
Wayne <kuanchieh1102@hotmail.com>
2016-03-12 01:12:14 +0800

    finish all function

--- HomeWork1/Controllers/客戶資料Controller.cs
+++ HomeWork1/Controllers/客戶資料Controller.cs
@@ -1,127 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using HomeWork1.Models;
 
 namespace HomeWork1.Controllers
 {
-    public class 客戶資料Controller : Controller
-    {
-        private 客戶資料Entities db = new 客戶資料Entities();
+   public class 客戶資料Controller: Controller
+   {
+      private 客戶資料Entities db = new 客戶資料Entities();
 
-        // GET: 客戶資料
-        public ActionResult Index()
-        {
-            return View(db.客戶資料.ToList());
-        }
+      public ActionResult 客戶關聯資料表() {
+         return View(db.vw客戶關聯資料統計表.ToList());
+      }
 
-        // GET: 客戶資料/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            if (客戶資料 == null)
-            {
-                return HttpNotFound();
-            }
-            return View(客戶資料);
-        }
+      // GET: 客戶資料
+      public ActionResult Index(string keyword) {
+         var data = db.客戶資料.Where(d => false == d.IsDelete).AsQueryable();
 
-        // GET: 客戶資料/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+         if (!string.IsNullOrEmpty(keyword)) {
+            data = data.Where(d => d.客戶名稱.Contains(keyword));
+         }
 
-        // POST: 客戶資料/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
-        {
-            if (ModelState.IsValid)
-            {
-                db.客戶資料.Add(客戶資料);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+         return View(data.ToList());
+      }
 
-            return View(客戶資料);
-        }
+      // GET: 客戶資料/Details/5
+      public ActionResult Details(int? id) {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         客戶資料 客戶資料 = db.客戶資料.Find(id);
+         if (客戶資料 == null) {
+            return HttpNotFound();
+         }
+         return View(客戶資料);
+      }
 
-        // GET: 客戶資料/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            if (客戶資料 == null)
-            {
-                return HttpNotFound();
-            }
-            return View(客戶資料);
-        }
+      // GET: 客戶資料/Create
+      public ActionResult Create() {
+         return View();
+      }
 
-        // POST: 客戶資料/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(客戶資料).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(客戶資料);
-        }
-
-        // GET: 客戶資料/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            if (客戶資料 == null)
-            {
-                return HttpNotFound();
-            }
-            return View(客戶資料);
-        }
-
-        // POST: 客戶資料/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            db.客戶資料.Remove(客戶資料);
+      // POST: 客戶資料/Create
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料) {
+         if (ModelState.IsValid) {
+            db.客戶資料.Add(客戶資料);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
+         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+         return View(客戶資料);
+      }
+
+      // GET: 客戶資料/Edit/5
+      public ActionResult Edit(int? id) {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         客戶資料 客戶資料 = db.客戶資料.Find(id);
+         if (客戶資料 == null) {
+            return HttpNotFound();
+         }
+         return View(客戶資料);
+      }
+
+      // POST: 客戶資料/Edit/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料) {
+         if (ModelState.IsValid) {
+            db.Entry(客戶資料).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(客戶資料);
+      }
+
+      // GET: 客戶資料/Delete/5
+      public ActionResult Delete(int? id) {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         客戶資料 客戶資料 = db.客戶資料.Find(id);
+         if (客戶資料 == null) {
+            return HttpNotFound();
+         }
+         return View(客戶資料);
+      }
+
+      // POST: 客戶資料/Delete/5
+      [HttpPost, ActionName("Delete")]
+      [ValidateAntiForgeryToken]
+      public ActionResult DeleteConfirmed(int id) {
+         客戶資料 客戶資料 = db.客戶資料.Find(id);
+         客戶資料.IsDelete = true;
+         db.SaveChanges();
+         return RedirectToAction("Index");
+      }
+
+      protected override void Dispose(bool disposing) {
+         if (disposing) {
+            db.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+
+   }
 }
